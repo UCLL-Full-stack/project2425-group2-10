71089d2003db26b2fa4404952df9dd835d4b40d1
thiminhnguyetduong <thiminhnguyet.duong@student.ucll.be>
2024-11-03 23:41:16 +0100

    Fix errors in backend

--- Front-end/applywise/types/index.d.ts
+++ Front-end/applywise/types/index.d.ts
@@ -1,32 +1,37 @@
+// front-end/applywise/types/index.d.ts
+
 export type User = {
-    id?: number;
-    name?: string;
-    age?: number;
-    email?: string;
-    password?: string;
-  };
-  
-  export type Team = {
-    id?: number;
-    name?: string;
-    points?: number;
-  };
-  
-  export type Job = {
-    id?: number;
-    title?: string;
-    description?: string;
-  };
-  
-  export type StatusMessage = {
-    message: string;
-    type: "error" | "success";
-  };
-  
-  export type Skill = {
-    id?: number;
-    name?: string;
-    description?: string;
-    category?: string;
-    level?: string;
-  };
+  id?: number;
+  name?: string;
+  age?: number;
+  email?: string;
+  password?: string;
+};
+
+export type Team = {
+  id?: number;
+  name?: string;
+  points?: number;
+};
+
+export type Job = {
+  id?: number;
+  companyId?: number;
+  title?: string;
+  description?: string;
+  deadline?: string; // ISO date string
+  requiredSkills?: string[];
+};
+
+export type StatusMessage = {
+  message: string;
+  type: "error" | "success";
+};
+
+export type Skill = {
+  id?: number;
+  name?: string;
+  description?: string;
+  category?: string;
+  level?: string;
+};
